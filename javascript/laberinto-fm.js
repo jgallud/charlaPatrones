a7e3054d5859043ea28de4840d16e60510158181
jgallud <jgallud@gmail.com>
2016-04-14 08:09:02 +0200

    maze nv

--- javascript/laberinto-fm.js
+++ javascript/laberinto-fm.js
@@ -6,8 +6,10 @@ function JuegoLaberinto(){
 		var h1=new Habitacion();
 		var h2=new Habitacion();
 		var puerta=new Puerta(h1,h2);
-		h1.lados[0]=puerta;
-		h2.lados[2]=puerta;
+		//h1.lados[0]=puerta;
+		//h2.lados[2]=puerta;
+		h1.norte=puerta;
+		h2.sur=puerta;
 		this.laberinto.agregarHabitacion(h1);
 		this.laberinto.agregarHabitacion(h2);
 		return this.laberinto
@@ -34,17 +36,39 @@ function Habitacion(){
 	this.nombre="Habitacion";
 	this.id=null;
 	this.lados=[];
+	this.norte=null;
+	this.este=null;
+	this.oeste=null;
+	this.sur=null;
 	this.agregarLado=function(em){
 		this.lados.push(em);
 	}
-	this.lados.push(new Pared());
-	this.lados.push(new Pared());
-	this.lados.push(new Pared());
-	this.lados.push(new Pared());	
+	//this.lados.push(new Pared());
+	//this.lados.push(new Pared());
+	//this.lados.push(new Pared());
+	//this.lados.push(new Pared());	
+	this.norte=new Pared();
+	this.este=new Pared();
+	this.oeste=new Pared();
+	this.sur=new Pared();
 }
 
 function Puerta(h1,h2){
 	this.nombre="Puerta";
 	this.h1=h1;
 	this.h2=h2;
+}
+
+function Norte(){
+	this.nombre="Norte";
+}
+
+function Este(){
+	this.nombre="Este";
+}
+function Oeste(){
+	this.nombre="Oeste";
+}
+function Sur(){
+	this.nombre="Sur";
 }
